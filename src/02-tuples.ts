7cb4f92a771fcf81512133a67c6a7a1a054173d4
angel melendez <angelmelen88@gmail.com>
2024-05-01 16:31:41 -0600

    feat:tuples

--- src/02-tuples.ts
+++ src/02-tuples.ts
@@ -1,8 +1,16 @@
+import { log } from "console";
+
 const prices: (number | string)[] = [1,2,3,4,2,1, 'as'];
 
 prices.push(1);
 prices.push('1');
 
-let user: [string, number] = ['angel', 12];
-user = ['luis', 13];
-user = ['12',5];
+let user: [string, number, boolean];
+// user = ['luis', 13];
+// user = ['12',5];
+
+
+user = ['angel',12 , true];
+const [username, age] = user;
+console.log(username);
+console.log(age);
